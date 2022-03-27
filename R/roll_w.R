5786a164e2f10edcc66ce6bfdb3fedd7734fb4c8
kwlee58 <kwlee58@gmail.com>
2022-03-27 10:05:05 +0900

    cleaned

--- R/roll_w.R
+++
@@ -1,9 +0,0 @@
-roll_w <-
-function() {
-    die <- 1:6
-    dice <- sample(die, 
-                   size = 2, 
-                   replace = TRUE, 
-                   prob = c(rep(1/8, 5), 3/8))
-    sum(dice)
-    }
--- R/shuffle.R
+++
@@ -1,9 +0,0 @@
-deal <-
-function(cards) { 
-  cards[1, ]
-  }
-shuffle <-
-function(cards) {
-  random <- sample(1:52, size = 52)
-  cards[random, ]
-}
