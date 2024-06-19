d6d5156b288fd9b2c3295d50ec68aa470063b4a4
coop711 <coop711@icloud.com>
2024-06-19 19:26:03 +0900

    P2P added

---
+++ Team_Project_P2P.R
@@ -0,0 +1,15 @@
+library(readxl)
+library(magrittr)
+data <- 
+  read_excel("./data/Team_Project_P2P.xlsx", 
+             range = "B1:H158",
+             col_names = TRUE)
+data <- data[, c(1, 2, 6)]
+names(data) <- c("name", "id", "scores")
+mean_scores <- 
+  tapply(data$scores, data$id, mean) %>%
+  round(digits = 2)
+str(mean_scores)
+mean_score <- 
+  data.frame(id = names(mean_scores), scores = mean_scores, row.names = NULL)
+write.csv(mean_score, file = "./data/Team_Project_P2p_scores.csv", fileEncoding = "UTF-8")  
